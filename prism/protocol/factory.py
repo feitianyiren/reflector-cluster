94ed4f6e28f4eb365e0df0343ce8f2716c2699c8
Kay Kurokawa <kay@lbry.io>
2017-08-25 14:05:09 -0700

    add factory for stream client

--- prism/protocol/factory.py
+++ prism/protocol/factory.py
@@ -5,6 +5,7 @@ from twisted.internet import defer
 
 from prism.protocol.server import ReflectorServerProtocol, enqueue_blob
 from prism.protocol.client import BlobReflectorClient
+from prism.protocol.stream_client import StreamReflectorClient
 from prism.config import get_settings
 
 log = logging.getLogger(__name__)
@@ -66,3 +67,51 @@ class PrismClientFactory(ClientFactory):
         p.blob_hashes_to_send = self.blobs
         self.p = p
         return p
+
+class PrismStreamClientFactory(ClientFactory):
+    protocol = StreamReflectorClient
+
+    def __init__(self, storage, sd_blob, blobs):
+        self.storage = storage
+        self.sd_blob = sd_blob
+        self.blobs = blobs
+        self.protocol_version = 1
+        self.p = None
+
+    def buildProtocol(self, addr):
+        p = self.protocol(self.sd_blob, self.blobs)
+        p.factory = self
+        p.addr = addr
+        p.protocol_version = self.protocol_version
+        self.p = p
+        return p
+
+
+@defer.inlineCallbacks
+def build_prism_stream_client_factory(sd_hash, blob_storage):
+    """ 
+    Build a prism client factory
+
+    sd_hash - sd_hash of stream to send
+    blob_storage - blob storage class
+    """
+    blob_exists = yield blob_storage.blob_exists(sd_hash)
+    if not blob_exists:
+        raise Exception("blob does not exist in cluster")
+
+    blob_forwarded = yield blob_storage.blob_has_been_forwarded_to_host(sd_hash)
+    if blob_forwarded:
+        raise Exception("blob has been forwarded") 
+
+    sd_blob = yield blob_storage.get_blob(sd_hash)
+    if not sd_blob.verified:
+        raise Exception("cannot send unverified sd blob")
+
+    blobs = yield blob_storage.get_blobs_for_stream(sd_hash)
+    for b in blobs:
+        if not b.verified:
+            raise Exception("blob %s is not verified", b.blob_hash)
+
+
+    defer.returnValue(PrismStreamClientFactory(blob_storage, sd_blob, blobs))
+
