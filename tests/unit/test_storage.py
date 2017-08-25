920662961d0121b8484afdb0ad2141d1acd62398
Kay Kurokawa <kay@lbry.io>
2017-08-25 13:07:07 -0700

    add some more unit tests for storage, use fakeredis in ClusterStorage

--- tests/unit/test_storage.py
+++ tests/unit/test_storage.py
@@ -1,7 +1,6 @@
 import shutil
 import tempfile
 from os import path
-import fakeredis
 
 from twisted.trial import unittest
 from twisted.internet import defer
@@ -12,8 +11,7 @@ from prism.protocol.blob import BlobFile
 class TestClusterStorage(unittest.TestCase):
     def setUp(self):
         self.db_dir = tempfile.mkdtemp()
-        self.cs = ClusterStorage(self.db_dir)
-        self.cs.db.db = fakeredis.FakeStrictRedis()
+        self.cs = ClusterStorage(self.db_dir, 'fake')
 
     def tearDown(self):
         shutil.rmtree(self.db_dir)
@@ -31,6 +29,12 @@ class TestClusterStorage(unittest.TestCase):
         out = yield self.cs.delete(blob_hash)
         out = yield self.cs.blob_exists(blob_hash)
         self.assertEqual(False, out)
+
+        # some non existing blob
+        blob_hash = '1ac46ae5445eb2d26ff41739440ac92d240fdade9a34d38f87f5b47154f6edc95f637a1a2cdb3ae60aa2c2ef91533d11'
+        out = yield self.cs.get_blob(blob_hash)
+        self.assertEqual(None, out.length)
+        self.assertFalse(out._verified)
 
 if __name__=='__main__':
     unittest.main()
