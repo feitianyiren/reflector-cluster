35e20a6063ea96d4d48a96d3b4437a016bf8bee9
Kay Kurokawa <kay@lbry.io>
2017-09-04 12:21:39 -0400

    work on enqueue_stream and process_stream

--- prism/protocol/task.py
+++ prism/protocol/task.py
@@ -8,7 +8,7 @@ from rq import Queue
 from redis.exceptions import ConnectionError
 from rq.timeouts import JobTimeoutException
 
-from twisted.internet import defer
+from twisted.internet import defer, task
 
 from prism.storage.storage import ClusterStorage
 from prism.config import get_settings
@@ -102,6 +102,51 @@ def process_blob(blob_hash, db_dir, client_factory_class, redis_address, host_in
     except Exception as err:
         log.exception("Job (pid %s) encountered unexpected error")
         return sys.exit(1)
+
+
+def process_stream(sd_hash, db_dir, client_factory_class, redis_address, host_infos, setup_d=None):
+    log.info("processing stream %s",sd_hash)
+    host, port, host_blob_count = host_infos
+    blob_storage = ClusterStorage(db_dir)
+    blob_storage.db.db = get_redis(redis_address)
+
+    from twisted.internet import reactor
+    def _process_stream(factory):
+        @defer.inlineCallbacks
+        def on_finish(result):
+            blob_hashes_sent = factory.p.blob_hashes_sent
+            for blob_hash in blob_hashes_sent:
+                yield update_sent_blob(blob_hash, host, blob_storage)
+
+            connection.disconnect()
+            reactor.fireSystemEvent("shutdown")
+        factory.on_connection_lost_d.addCallback(on_finish)
+        try:
+            connection = reactor.connectTCP(host, port, factory)
+        except JobTimeoutException:
+            log.error("Failed to forward %s --> %s", blob_hash[:8], host)
+            return sys.exit(0)
+        except Exception as err:
+            log.exception("Job (pid %s) encountered unexpected error")
+            return sys.exit(1)
+    if setup_d is not None:
+        d = setup_d()
+    else:
+        d = defer.succeed(True)
+    d.addCallback(lambda _: client_factory_class(sd_hash,blob_storage))
+    d.addCallback(lambda factory: _process_stream(factory))
+    reactor.run()
+    return sys.exit(0)
+
+@retry_redis
+def enqueue_stream(sd_hash, num_blobs_in_stream, db_dir, client_factory_class, redis_address=settings['redis server'],
+                   host_getter=next_host):
+
+    timeout = (num_blobs_in_stream+1)*30
+    redis_connection = get_redis(redis_address)
+    q = Queue(connection=redis_connection)
+    host_infos = host_getter(redis_connection)
+    q.enqueue(process_stream, sd_hash, db_dir, client_factory_class, redis_address, host_infos, timeout=timeout)
 
 @retry_redis
 def enqueue_blob(blob_hash, db_dir, client_factory_class, redis_address=settings['redis server'], host_getter=next_host):
