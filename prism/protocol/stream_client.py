10e1deebb587bef70b464252ac1c24dfc168c9dd
Kay Kurokawa <kay@lbry.io>
2017-09-29 15:52:14 -0400

    close_read_handle() no longer supported on lbrynet 0.17.0rc8

--- prism/protocol/stream_client.py
+++ prism/protocol/stream_client.py
@@ -95,7 +95,7 @@ class StreamReflectorClient(Protocol):
 
     def set_not_uploading(self):
         if self.next_blob_to_send is not None:
-            self.next_blob_to_send.close_read_handle(self.read_handle)
+            self.read_handle.close()
             self.read_handle = None
             self.next_blob_to_send = None
         self.file_sender = None
