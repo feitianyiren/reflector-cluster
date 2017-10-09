730817ee91d32fff964b04433727b87e61c2b7a8
Kay Kurokawa <kay@lbry.io>
2017-10-09 11:02:03 -0400

    lower default max blob per host

--- prism/config.py
+++ prism/config.py
@@ -34,7 +34,7 @@ def get_settings():
         HOSTS: [
             "jack.lbry.tech",
         ],
-        MAX_BLOBS_PER_HOST: 500000, # assuming 1 terabyte disk / 2 mb blobs
+        MAX_BLOBS_PER_HOST: 490000, # assuming 1 terabyte disk / 2 mb blobs
         BLOB_DIR: os.path.expanduser("~/.prism"),
         REDIS_SERVER: "localhost",
         ENQUEUE_ON_STARTUP: True
