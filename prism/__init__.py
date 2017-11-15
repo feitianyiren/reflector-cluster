408efb295b7eef24ff36a36ad786c9000d2203d1
Jack Robison <jackrobison@lbry.io>
2017-11-15 09:23:09 -0500

    catch ReactorAlreadyInstalledError

--- prism/__init__.py
+++ prism/__init__.py
@@ -1,4 +1,10 @@
-from twisted.internet import selectreactor
-selectreactor.install()
+from twisted.internet import selectreactor, error
+try:
+    selectreactor.install()
+except error.ReactorAlreadyInstalledError:
+    from twisted.internet import reactor
+    if reactor.__class__.__name__ != "SelectReactor":
+        print "Failed to install SelectReactor because %s is already installed" % \
+              reactor.__class__.__name__
 
 __version__ = "0.0.1"
