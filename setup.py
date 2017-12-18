bc63a1f96d03b522a2f3cf335c333e64ff80a412
Jack Robison <jackrobison@lbry.io>
2017-12-18 11:34:48 -0500

    Bump version 0.0.2rc24 --> 0.0.2rc25
    Signed-off-by: Jack Robison <jackrobison@lbry.io>

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requires = [
     'psutil==5.2.2',
     'lbryschema==0.0.15rc2',
     'lbryum==3.2.0rc7',
-    'lbrynet==0.19.0rc19',
+    'lbrynet==0.19.0rc20',
 ]
 console_scripts = [
     'prism-server = prism.server:main',
